cb83df20d73e3a34a64e9b6a6c2dde14d27d8d1c
Yauheni <yauheni@yauheni-hp-pavilion14.zte.com.cn>
2021-03-25 16:42:59 +0300

    create message with API

--- TestWebSite/TestWebSite/settings.py
+++ TestWebSite/TestWebSite/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'TestWebSite'
+    'TestWebSite',
+    'rest_framework',
+    'messages.apps.MessagesConfig'
 ]
 
 MIDDLEWARE = [
