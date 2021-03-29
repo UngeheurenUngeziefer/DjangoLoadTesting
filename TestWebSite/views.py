e04b59559649c37a7a14f9601c4595a9b7afccc8
Yauheni <yauheni@yauheni-hp-pavilion14.zte.com.cn>
2021-03-29 16:32:40 +0300

    html page

--- TestWebSite/views.py
+++ TestWebSite/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from messages.models import Message
 
 def index(request):
-    return render(request, 'TestWebSite/index.html')
+	objects = Message.objects.all()
 
+	context = {
+		'object_list': objects
+	}
 
-
-
+	return render(request, 'TestWebSite/index.html', context)
